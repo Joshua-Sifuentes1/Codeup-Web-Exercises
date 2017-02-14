27b74b298e3c204be586809551f1f14132fd4783
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-02-14 11:10:26 -0600

    Made changes to if-else.js. Added more variables to help clean up code and help with formatting.

--- public/js/if-else.js
+++ public/js/if-else.js
@@ -1,35 +1,57 @@
 "use strict";
 
-var average = (70 + 80 + 95) / 3;
+// Test average exercise.
 
-if (average > 80) {
+var testOne = 70,
+	testTwo = 80,
+	testThree = 95,
+	numberOfTests = 3,
+	requiredAverage = 80,
+	testSum = testOne + testTwo + testThree,
+	average = testSum / numberOfTests;
+
+if (average > requiredAverage) {
 	console.log("You're awesome!");
 } else {
 	console.log("You need to practice more.");
 }
 
+// Grocery shopping exercise.
 
+var discount = 0.65
 var cameronPurchase = 180;
 var ryanPurchase = 250;
 var georgePurchase = 320;
+var totalRequired = 200;
+var cameronTotal = cameronPurchase * discount;
+var ryanTotal = ryanPurchase * discount;
+var georgeTotal = georgePurchase * discount;
 
-if (cameronPurchase > 200) {
-	console.log("Discount is applied. Cameron spent $" + cameronPurchase + " and will have to pay a total of $" + (cameronPurchase * 0.65) + "." );
+if (cameronPurchase > totalRequired) {
+	console.log("Discount is applied. Cameron spent $" + cameronPurchase +
+	 " and will have to pay a total of $" + cameronTotal + "." );
 } else {
-	console.log("Cameron will need to purchase $" + (200 - cameronPurchase) + " more in groceries to have discount applied.")
+	console.log("Cameron will need to purchase $" + (totalRequired - cameronPurchase) +
+	 " more in groceries to have discount applied.")
 }
 
-if (ryanPurchase > 200) {
-	console.log("Discount is applied. Ryan spent $" + ryanPurchase + " and will have to pay a total of $" + (ryanPurchase * 0.65) + "." );
+if (ryanPurchase > totalRequired) {
+	console.log("Discount is applied. Ryan spent $" + ryanPurchase +
+	 " and will have to pay a total of $" + ryanTotal + "." );
 } else {
-	console.log("Ryan will need to purchase $" + (200 - ryanPurchase) + " more in groceries to have discount applied.")
+	console.log("Ryan will need to purchase $" + (totalRequired - ryanPurchase) +
+	 " more in groceries to have discount applied.")
 }
 
-if (georgePurchase > 200) {
-	console.log("Discount is applied. George spent $" + georgePurchase + " and will have to pay a total of $" + (georgePurchase * 0.65) + "." );
+if (georgePurchase > totalRequired) {
+	console.log("Discount is applied. George spent $" + georgePurchase +
+	 " and will have to pay a total of $" + georgeTotal + "." );
 } else {
-	console.log("George will need to purchase $" + (200 - georgePurchase) + " more in groceries to have discount applied.")
+	console.log("George will need to purchase $" + (totalRequired - georgePurchase) +
+	 " more in groceries to have discount applied.")
 }
+
+// Flip a coin exercise.
 
 var flipACoin = Math.floor(Math.random()* 2)
 
@@ -40,3 +62,4 @@ var flipACoin = Math.floor(Math.random()* 2)
 // }
 
 var whichPurchase = (flipACoin === 0) ? console.log("Buy a car!") : console.log("Buy a house!");
+
