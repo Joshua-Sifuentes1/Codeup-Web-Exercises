c98f15bca2437b2370af9f00378c5ebe384296d4
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-02-16 10:39:44 -0600

    Added for loop and forEach loop to file.

--- public/js/iterating.js
+++ public/js/iterating.js
@@ -1,15 +1,29 @@
 (function(){
-    "use strict";
+	"use strict";
 
-    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
-    var names = ["Thrall", "Illidan", "Malfurion", "Jaina"];
+	// TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+	var names = ["Thrall", "Illidan", "Malfurion", "Jaina"];
 
-    // TODO: Create a log statement that will log the number of elements in the names array.
-    console.log(names.length)
+	// TODO: Create a log statement that will log the number of elements in the names array.
+	console.log(names.length)
 
-    // TODO: Create log statements that will print each of the names array elements individually.
-    console.log(names[0]);
-    console.log(names[1]);
-    console.log(names[2]);
-    console.log(names[3]);
+	// TODO: Create log statements that will print each of the names array elements individually.
+	console.log(names[0]);
+	console.log(names[1]);
+	console.log(names[2]);
+	console.log(names[3]);
+
+	//for loop.
+	for (var i = 0; i < (names.length - 1); i++ ) {
+		
+		console.log(names[i]);
+	}
+
+	//forEach loop.
+	names.forEach(function(element, index, array) {
+
+		console.log(element);
+
+	});
 })();
+
