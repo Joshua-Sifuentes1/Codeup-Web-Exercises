4069be3346edf001b1b47375ff3218d1355a9a46
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-02-23 11:39:53 -0600

    made some changes to the file.

--- public/js/challenges.js
+++ public/js/challenges.js
@@ -1,14 +1,20 @@
-Write a program to toggle all characters in a string: in a given string, toggle all characters to their opposite case. Special characters will not change.
+// Write a program to toggle all characters in a string: in a given string, 
+// toggle all characters to their opposite case. Special characters will not change.
 
-Example input: "aBc12#""
-Example output: "AbC12#"
+// Example input: "aBc12#""
+// Example output: "AbC12#"
 
+function togglecase() {
+	
+}
 
+// Write a program that takes a given string appended with a number at the end.
+// The program will check the length of the string to verify the string is equal to the number appended.
+// If the number appended equals the string length, output "Yes". 
+// if the number appended does not equal the string length, output "No".
 
-Write a program that takes a given string appended with a number at the end. The program will check the length of the string to verify the string is equal to the number appended. If the number appended equals the string length, output "Yes". if the number appended does not equal the string length, output "No".
+// Example Input: "geek5"
+// Example Output: "Yes"
 
-Example Input: "geek5"
-Example Output: "Yes"
-
-Example Input: "codingchallenge25"
-Example Output: "No"
+// Example Input: "codingchallenge25"
+// Example Output: "No"
