19253bfc6b8cf6eb3a60732506616c4e0f57d80e
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-27 10:54:08 -0500

    Initial commit.

---
+++ public/counter.php
@@ -0,0 +1,21 @@
+<?php  
+
+function pageController()
+{
+
+}
+
+extract(pageController());
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Counter</title>
+	</head>
+	<body>
+		<a href="">Up</a>
+		<a href="">Down</a>
+
+	</body>
+</html>
