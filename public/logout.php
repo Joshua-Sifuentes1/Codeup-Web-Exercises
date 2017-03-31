2eeff97956c5dfd59bedf9de01dd14ea77596012
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-31 15:23:39 -0500

    completed exercise

--- public/logout.php
+++ public/logout.php
@@ -1,41 +1,26 @@
-<?php  
+<?php 
+ 
+require_once '../Input.php';
+require_once '../Auth.php';
+
 session_start();
 
-if (isset($_SESSION['logged_in_user'])) {
-	clearSession();
+$sessionId = session_id();
+
+if (!Auth::check()) {
 	header("Location: http://codeup.dev/login.php");
 	exit;
 } else {
-	header("Location: http://codeup.dev/login.php");
-	exit;
+	Auth::logout();
 }
-
-function clearSession()
-{
-    // clear $_SESSION array
-    session_unset();
-
-    // delete session data on the server
-    session_destroy();
-
-    // ensure client is sent a new session cookie
-    session_regenerate_id();
-
-    // start a new session - session_destroy() ended our previous session so
-    // if we want to store any new data in $_SESSION we must start a new one
-    session_start();
-}
-
-
-
-
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title></title>
+		<title>Logout</title>
 	</head>
 	<body>
-
+		<h1>You have been logged out!</h1>
+		<a href="http://codeup.dev/login.php">Go back to Login Page</a>
 	</body>
 </html>
