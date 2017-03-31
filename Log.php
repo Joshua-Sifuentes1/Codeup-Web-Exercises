4ab1996fdf79c75422957066b4d8357cd58a24f7
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-03-31 15:21:10 -0500

    completed file

---
+++ Log.php
@@ -0,0 +1,39 @@
+<?php  
+
+class Log {
+	public $filename;
+	public $handle;
+
+	public function __construct($prefix = 'log')
+	{	
+		$date = date("Y-m-d");
+
+		$this->filename = "$prefix-$date.log";
+
+		$this->handle = fopen($this->filename, 'a');
+	}
+
+	public function logMessage($logLevel, $message)
+	{
+		$timeStamp = date("Y-m-d H:i:s");
+
+		fwrite($this->handle, "$timeStamp [$logLevel] $message" . PHP_EOL);
+	}
+
+	public function info($message)
+	{
+		$this->logMessage("INFO", $message);
+	}
+
+	public function error($message)
+	{
+		$this->logMessage("ERROR", $message);
+	}
+	
+	public function __destruct(){
+
+		fclose($this->handle);
+	}
+}
+
+?>
